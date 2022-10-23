24911951d34e35866bb2985d5b74d2b6d53fcb69
Alexandre Agostinho <alexandre.agostinho@etu.uca.fr>
2022-10-23 02:04:48 +0200

    add chargement et sauvegarrde points

---
+++ src/carte.h
@@ -0,0 +1,17 @@
+/**
+* \file carte.h
+* \brief Gestion des points sur la carte adhérent
+* \author Alexandre Agostinho
+* \date 22/09/2022
+*/
+
+
+/**
+ * \brief chargement du fichier de points
+ */
+int chargPoints(int *tAdhe, int *tNPoints, int tMax);
+
+/**
+ * \brief sauvegarde du fichier de points
+ */
+int sauvgPoints(int *tAdhe, int *tNPoints, int tLog);
